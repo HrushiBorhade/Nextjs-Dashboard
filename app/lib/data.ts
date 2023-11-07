9327d95125e4942c4881e5f023b2c3563ab2d873
Hrushikesh Borhade <hrushiborhade123@gmail.com>
2023-11-07 19:34:17 +0530

    dashboard

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,9 +14,6 @@ export async function fetchRevenue() {
   noStore();
 
   try {
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log("Data fetch complete after 3 seconds.");
